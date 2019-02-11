7c683613adb8276228c998006e49e36258ea32fc
Madhur Dixit <dmadhur6@gmail.com>
2019-02-11 22:29:35 +0530

    Trailing 0's added to dataword

--- Error Control/CRC.c
+++ Error Control/CRC.c
@@ -5,9 +5,17 @@
 int main()
 {
     printf("Hello\n");
-    char dataword[100],genpoly[100];
+    char dataword[100],genpoly[100],trailz[100];
+    int ez=0,x=0;
     scanf("%s",&dataword);
     scanf("%s",&genpoly);
-    printf("%s - %s",dataword,genpoly);
+    ez=strlen(genpoly)-1;
+    printf("%s - %s---%d",dataword,genpoly,ez);
+    trailz[0]='0';
+    trailz[1]='\0';
+    for(x=0;x<ez;x++)
+        strcat(dataword,trailz);
+    printf("\ndataword after trail-%s",dataword);
+
     return(0);
 }

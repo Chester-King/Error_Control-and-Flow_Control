d0828aeb87e4a30eb1a808944d143fd358f07e3f
Madhur Dixit <dmadhur6@gmail.com>
2019-02-11 22:12:19 +0530

    CRC- Input Taken

--- Error Control/CRC.c
+++ Error Control/CRC.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+    printf("Hello\n");
+    char dataword[100],genpoly[100];
+    scanf("%s",&dataword);
+    scanf("%s",&genpoly);
+    printf("%s - %s",dataword,genpoly);
+    return(0);
+}
